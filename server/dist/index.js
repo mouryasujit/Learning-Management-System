350871c6eb96261be040885277dbf20c34097756
Sujitkumar Mourya <mouryasujit04@gmail.com>
2025-01-03 17:25:26 +0530

    Modifying vercel.json 8

--- server/dist/index.js
+++ server/dist/index.js
@@ -72,6 +72,7 @@ app.use((err, req, res, next) => {
     });
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on PORT: ${PORT}`);
+// });
+exports.default = app;
